4ba2af03ed0620eb4063c77b50c0e72812f0005b
daniel <dani.ajala@yahoo.com>
2021-11-12 16:41:06 -0300

    automaçao api OO

--- api/spec/post_session_spec.rb
+++ api/spec/post_session_spec.rb
@@ -1,4 +1,6 @@
-require "httparty"
+
+
+require_relative "routes/sessions"
 
 context "login com sucesso" do
 describe "POST /sessions" do
@@ -7,28 +9,45 @@ describe "POST /sessions" do
   before(:all) do
       
    
+  @result = Sessions.new.login("dani.ajala@yahoo.com","12345")
         
-        payload = {email:"dani.ajala@yahoo.com",password:"12345@"}
-    
-     @result =   HTTParty.post("http://rocklov-api:3333/sessions",
-            body:payload.to_json,
-            headers:{
-                "Content-Type":"application/json",
-            },
-        )
-        puts "teste"
   end
    
 it "valida status code" do
        
-    expect(@result.code).to eql 200
+    expect(@result.code).to eql 401
 
    end
 
 it "valida id do usuario"  do
-    expect(@result.parsed_response["_id"].length).to eql 24
-
-   end
-
-    end
+    expect(@result.parsed_response["error"]).to eql  "Unauthorized"
 end
+end
+end
+ 
+
+
+context "senha invalida" do
+  describe "POST /sessions" do
+    
+  
+    before(:all) do
+        
+     
+    @result = Sessions.new.login("dani.ajala@yahoo.com","1345")
+          
+    end
+     
+  it "valida id do usuario" do
+         
+      expect(@result.parsed_response["error"]).to eql "Unauthorized"
+  
+     end
+  
+  it "valida id do usuario"  do
+      expect(@result.parsed_response["error"]).to eql  "Unauthorized"
+  end
+  end
+  end
+   
+  
---
+++ api/spec/routes/sessions.rb
@@ -0,0 +1,17 @@
+
+require "httparty"
+class Sessions
+   
+    include HTTParty
+    def login(email,pass)   
+        payload = {email:email,password:pass}
+    
+    return self.class.post("http://rocklov-api:3333/sessions",
+            body:payload.to_json,
+            headers:{
+                "Content-Type":"application/json",
+            },
+        )
+   end
+     end
+    
